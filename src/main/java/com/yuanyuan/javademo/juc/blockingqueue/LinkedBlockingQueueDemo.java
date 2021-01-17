455061ccdfc9965ee4693e95534562bdc8966964
fayuan.peng <fa@mycredigo.com>
2021-01-17 12:29:57 +0800

    commit

---
+++ src/main/java/com/yuanyuan/javademo/juc/blockingqueue/LinkedBlockingQueueDemo.java
@@ -0,0 +1,16 @@
+package com.yuanyuan.javademo.juc.blockingqueue;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * @author FAYUAN.PENG
+ * @version \$Id: LinkedBlockingQueueDemo.java,  2021-01-17 12:20 FAYUAN.PENG Exp $$
+ */
+public class LinkedBlockingQueueDemo {
+
+    public static void main(String[] args) {
+        BlockingQueue<String> blockingQueue = new LinkedBlockingQueue<>();
+    }
+
+}
